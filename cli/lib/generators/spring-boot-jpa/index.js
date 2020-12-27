9105a1dc9345d8be30534cef1582f1b38ff5f4cb
Efe Kaptan <efekaptan@gmail.com>
2020-12-27 19:56:49 +0100

    Entity created

--- cli/lib/generators/spring-boot-jpa/index.js
+++ cli/lib/generators/spring-boot-jpa/index.js
@@ -1,5 +1,29 @@
+import { createFile, createFolder } from '../../util/file';
+import { join } from 'path';
+import { render } from '../../util/template';
+
 export default function generate(config) {
     console.log('spring-boot-jpa generator started');
     console.log(config);
+    createEntities(config);
     return config;
+}
+
+function createEntities(config) {
+    const { serverPackageName, entities, packageFolder } = config;
+    createFolder(join(packageFolder, 'pojo'));
+
+    entities.forEach((entity) => {
+        createEntity(entity, serverPackageName, packageFolder);
+    });
+}
+
+function createEntity(entity, serverPackageName, packageFolder) {
+    const templateDirectory = join(__dirname, 'template');
+    const { name } = entity;
+    const entityFile = render(join(templateDirectory, 'pojo/entity.java'), {
+        serverPackageName,
+        ...entity
+    });
+    createFile(join(packageFolder, `pojo/${name}.java`), entityFile);
 }
---
+++ cli/lib/generators/spring-boot-jpa/template/pojo/entity.java
@@ -0,0 +1,24 @@
+package {{serverPackageName}};
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class {{name}} {
+    {{#each columns}}
+    {{#if isPrimary}}
+    @Id
+    @GeneratedValue
+    {{/if}}
+    {{#unless isPrimary}}
+    
+    @Column
+    {{/unless}}
+    private {{type}} {{name}};
+    {{/each}}
+}
