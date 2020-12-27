8bd9addf39dcb2491326a9d818c743d179ce30f5
Efe Kaptan <efekaptan@gmail.com>
2020-12-27 09:44:54 +0100

    Copy base folder implemented

--- cli/lib/generators/spring-boot-core/index.js
+++ cli/lib/generators/spring-boot-core/index.js
@@ -1,3 +1,20 @@
+import { createFolder, copy } from '../../util/file';
+import { join } from 'path';
+
 export default function generate(config) {
     console.log('spring-boot-core generator started');
+    const { directory } = config;
+    const serverDirectory = join(directory, 'server');
+    createServerFolder(serverDirectory);
+
+    const templateDirectory = join(__dirname, 'template');
+    copyBaseFolder(serverDirectory, templateDirectory);
+}
+
+function createServerFolder(serverDirectory) {
+    createFolder(serverDirectory);
+}
+
+function copyBaseFolder(serverDirectory, templateDirectory) {
+    copy(join(templateDirectory, 'base'), serverDirectory);
 }
